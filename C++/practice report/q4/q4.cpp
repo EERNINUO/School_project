be704db83fbdabd7a1aa0f8cc42859d64bc83b4f
EERNINUO <18902670809@163.com>
2024-07-08 23:05:31 +0800

    q4 finish

--- C++/practice report/q4/q4.cpp
+++ C++/practice report/q4/q4.cpp
@@ -8,12 +8,12 @@ struct nums{
 
 static nums * head = NULL;			//存储链表头指针
 
-bool in(int num,nums * array){
+bool in(int num,nums * head){			//
     nums *current_p_in = head;			//
 	if(head != NULL){
 		while(current_p_in->next != NULL){
-			if(num == array->number) return true;		//如果当前数为目标数则返回真
-			else current_p_in = array->next;		//如果当前数不为目标数则移至下一个节点
+			if(num == current_p_in->number) return true;		//如果当前数为目标数则返回真
+			else current_p_in = current_p_in->next;		//如果当前数不为目标数则移至下一个节点
 		}
 	}
     return false;						//如果链表中无目标数则返回假
@@ -24,11 +24,13 @@ bool prime(int num){               //计算输入数据是不是素数
 	nums *prev,*malloc_return;
     if(in(num,head)) return true;
     else{
-		for(int i=2;i<num;i++){				//遍历比目标数小的数
+		for(int i=2;i<(num/2);i++){				//遍历比目标数小的数
 			if(num%i == 0) 	return false;	//判断目标数是否有其他因数，如果有，返回false
         }
 		malloc_return = (nums *)malloc(sizeof(nums));//创建一个新的节点
-		if(head == NULL) head = malloc_return;
+		if(head == NULL){
+			head = malloc_return;
+			}
 		else{
 			while(current_p_prime != NULL){
 				prev = current_p_prime;
@@ -52,32 +54,25 @@ void clean_list(void){
 }
 
 void gotbah(int number){
-	for(int i=0;i<number;i++){
-		if(number%i == 0){
-			if(prime(i) && prime(number/i));
+	for(int i=2;i<(number/2);i++){				//遍历比目标数小的所有值
+		if(prime(i)&&prime(number-i)){
+			printf("%d=%d+%d\n",number,i,number-i);
 		}
 	}
 }
 
 int main(){
     int input_num;
-	
     cout<< "Please input a even number not less than 6: ";
     cin>>input_num;
-	while (input_num != 0){
-		if(prime(input_num)) cout<<"yes"<<endl;
-		cout<< "Please input a even number not less than 6: ";
-		cin>>input_num;
-	}
+		if(input_num >= 6 && input_num%2 ==0)gotbah(input_num);
+		else if(input_num < 6) cout<< "Please input a number not less than 6!";
+		else if(input_num%2 != 0) cout<< "Please input a even number!";
+		else{
+			cout<< "Unknown error!";
+			clean_list();
+			exit(EXIT_FAILURE);
+		}
 	clean_list();
-	
-	/*
-    if(num >= 6 && num%2 ==0) cout<<1;
-    else if(num < 6) cout<< "Please input a number not less than 6!";
-    else if(num%2 != 0) cout<< "Please input a even number!";
-    else{
-        cout<< "Unknown error!";
-        exit(EXIT_FAILURE);
-    }
-    return EXIT_SUCCESS;*/
+    return EXIT_SUCCESS;
 }
